86b3a43d757dbfae7e0333cbfbbe913bf8e6f9a4
Clara Ni <clara.ni@student-cs.fr>
2018-11-19 17:03:10 +0100

    Commit de Clara on a des dates aléatoires qui marchent et c'est le dernier de la journée !!!

--- visualizer/db/candidats/gen_random_date.py
+++ visualizer/db/candidats/gen_random_date.py
@@ -5,15 +5,17 @@ import random
 1970 correspond à l'origine des temps (on ne peut pas atteindre des dates avant"""
 
 def date_aleatoire_entre_start_end(start_date, end_date):
+    '''crée une date aléatoire entre les dates start et end (1970 étant l'origine des temps, on ne peut pas
+    atteindre les dates antérieures)
+    :param start_date : str représentant la date de début
+    :param end_date : str représentant la date de fin
+    :return date : str représentant la date inventée
+    '''
     format = '%d/%m/%Y %I:%M %p'
     nb = random.random()
-        #on passe les chaînes de caractères en datetime
-        #il faut qu'elles soient données dans le format spécifié
     start_date = time.mktime(time.strptime(start_date, format))
     end_date = time.mktime(time.strptime(end_date, format))
-        #on calcule la nouvelle date
     date = start_date + nb * (end_date - start_date)
-        #on la transforme en string
     return time.strftime(format, time.localtime(date))
 
 
@@ -30,22 +32,26 @@ Pour atteindre des dates inférieures, il faut ruser (sinon "mktime argument out
 (au mois de septembre 2018) aléatoires"""
 
 def dates_aleatoires_naissance_entretien():
+    '''retourne deux dates crées aléatoirement corresondant à la date de naissance et à la date d'entretien
+    :return [dateNaissance, dateEntretien] : liste de str contenant la date de naissance (comprise entre 1970 et 1998) et la date d'entretien (mois de septembre 2018)'''
     dateNaissance = date_aleatoire_entre_start_end("01/01/1970 01:30 PM", "31/12/1998 11:59 PM")[:10]
-        #on récupère seulement la date
     jour_entretien = random.randint(0,30)
     dateEntretien = str(jour_entretien) + "/09/2018"
     return [dateNaissance, dateEntretien]
 
-#print(dates_aleatoires_naissance_entretien())
+print(type(dates_aleatoires_naissance_entretien()))
 
 """on crée une fonction qui crée aléatoirement une date de dépôt de fichiers, comprise dans les 5 jours
 suivant la date de l'entretien"""
 
 def date_depot_fichier(date_entretien):
+    '''retourne une date de dépôt choisie aléatoirement dans les 5 jours suivant l'entretien
+    :param date_entretien : str représentant la date d'entretien choisie aléatoirement
+    :return date_depot : str représentant la date de dépôt'''
     jour_entretien_str = ''
     i = 0
     while date_entretien[i] != "/" :
-        jour_entretien_str.append(date_entretien[i])
+        jour_entretien_str += date_entretien[i]
         i += 1
     jour_entretien = int(jour_entretien_str)
     jour_depot = random.randint(0,5)
@@ -56,4 +62,4 @@ def date_depot_fichier(date_entretien):
     return date_depot
 
 #date_entretien = "29/09/2018"
-#print(date_depot_fichier(date_entretien))
+#print(type(date_depot_fichier(date_entretien)))
--- visualizer/db/candidats/gen_random_date_test.py
+++ visualizer/db/candidats/gen_random_date_test.py
@@ -1,4 +1,16 @@
 import pytest
+import visualizer.db.candidats.gen_random_date as grd
 
-from gen_random_date
+def test_date_aleatoire_entre_start_end():
+    assert type(grd.date_aleatoire_entre_start_end()) == str
 
+def test_dates_aleatoires_naissance_entretien():
+    assert type(grd.dates_aleatoires_naissance_entretien()) == list
+
+def test_date_depot_fichier():
+    assert type(grd.date_depot_fichier()) == str
+    dates = []
+    for k in range (31):
+        dates.append([str(k)+"/09/2018"])
+    for date_entretien in dates:
+        assert grd.date_depot_fichier(date_entretien) >= date_entretien
