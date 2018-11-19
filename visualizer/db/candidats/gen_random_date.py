de22b164b7fe3e737eeaac46c634720cf3a66ed3
Clara Ni <clara.ni@student-cs.fr>
2018-11-19 15:15:58 +0100

    Commit de Clara on a des dates aléatoires !!!

--- visualizer/db/candidats/gen_random_date.py
+++ visualizer/db/candidats/gen_random_date.py
@@ -1,9 +1,8 @@
 import time
 import random
 
-"""on écrit un programe qui crée des dates de naissance aléatoires entre 1970 et 1998
-1970 correspond à l'origine des temps"""
-
+"""on écrit un programe qui crée des dates aléatoires entre la date start et la date end
+1970 correspond à l'origine des temps (on ne peut pas atteindre des dates avant"""
 
 def date_aleatoire_entre_start_end(start_date, end_date):
     format = '%d/%m/%Y %I:%M %p'
@@ -26,21 +25,31 @@ Pour atteindre des dates inférieures, il faut ruser (sinon "mktime argument out
 1970 correspond à l'origine des temps"""
 
 
-"""on écrit un programe qui crée une date de naissance (entre 1970 et 1998), 
-une date d'entretien (au mois de septembre 2018) et une date de dépôt de l'exercice (dans les 5 jours 
-qui suivent l'entretien) aléatoires"""
 
-def dates_aleatoires():
+"""on écrit un programe qui crée une date de naissance (entre 1970 et 1998) et une date d'entretien
+(au mois de septembre 2018) aléatoires"""
+
+def dates_aleatoires_naissance_entretien():
     dateNaissance = date_aleatoire_entre_start_end("01/01/1970 01:30 PM", "31/12/1998 11:59 PM")[:10]
+        #on récupère seulement la date
     jour_entretien = random.randint(0,30)
     dateEntretien = str(jour_entretien) + "/09/2018"
+    return [dateNaissance, dateEntretien, ]
+
+#print(dates_aleatoires_naissance_entretien())
+
+"""on crée une fonction qui crée aléatoirement une date de dépôt de fichiers, comprise dans les 5 jours
+suivant la date de l'entretien"""
+def date_depot_fichier(date_entretien):
+    jour_entretien = int(date_entretien[:2])
     jour_depot = random.randint(0,5)
+    print("jour_entretien = {}".format(jour_entretien))
+    print("jour_depôt = {}".format(jour_depot))
     if jour_entretien+jour_depot <= 30:
-        dateDepot = str(jour_entretien+jour_depot) + "/09/2018"
+        date_depot = str(jour_entretien+jour_depot) + "/09/2018"
     else:
-        dateDepot = str(jour_entretien+jour_depot % 30) + "/10/2018"
-    return [dateNaissance, dateEntretien, dateDepot]
+        date_depot = str((jour_entretien+jour_depot)%30) + "/10/2018"
+    return date_depot
 
-
-
-print (dates_aleatoires())
+date_entretien = "29/09/2018"
+print(date_depot_fichier(date_entretien))
