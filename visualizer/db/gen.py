9def4cdbc8545cda393926179ece4d5c28747870
CamilleBouyssel <camille.bouyssel@student-cs.fr>
2018-11-19 15:43:15 +0100

    generate database

---
+++ visualizer/db/gen.py
@@ -0,0 +1,134 @@
+#LIBRARIES
+import json
+import random as rd
+import numpy as np
+import visualizer.db.get_random_date as grd
+
+
+#On créer des candidats
+nombre_candidats=1
+
+
+#On formalise l'ecriture des noms et prenoms
+def formalise_nom(nom):
+    nom=nom.replace(' ','')
+    nom=str(nom)
+    maj=nom[0].upper()
+    min=nom[1:].lower()
+    return(maj+min)
+
+#On récupère des prénoms
+prenoms=open("prenoms.txt",'r').readlines()
+
+liste_prenoms=[]
+for ligne in prenoms:
+    ligne=ligne.replace('\n','')#on enlève les retours à la ligne
+    lin=[]
+    mot=''
+    for car in ligne:
+        if car =="\t":
+            lin.append(mot)
+            mot=''
+        else:
+            mot+=car
+    lin.append(mot)
+    if float(lin[3])>0 and '�' not in lin[0]: #la freqence du nom est non nulle et que � n'est pas dans le nom
+        liste_prenoms.append(formalise_nom(lin[0]))#on ajoute un prénom à la liste des prénoms
+
+
+
+#On récupère des noms
+noms=open("noms.txt",'r').readlines()
+liste_noms=[]
+for ligne in noms:
+    car=0
+    while ligne[car] != '\t':
+        car+=1
+    liste_noms.append(formalise_nom(ligne[:car]))
+
+
+
+#On rècupère une liste de villes
+villes=open("villes_france.csv", "r").readlines()
+liste_villes=[]
+for ville in villes:
+    rang_premiere_virgule=0
+    while ville[rang_premiere_virgule]!=',':
+        rang_premiere_virgule+=1
+    rang_min=rang_premiere_virgule+1
+    while ville[rang_min]!=',':
+        rang_min+=1
+    rang_max=rang_min+1
+    while ville[rang_max]!=',':
+        rang_max+=1
+    ville=ville[rang_min+1:rang_max]
+    ville=ville[1:-1]#On enlève les doubles guillemets
+    liste_villes.append(formalise_nom(ville))
+
+#Liste de lieux de RDV
+liste_lieu_rdv=['Eiffel','Bouygues','Breguet']
+
+
+##############On créer des fichiers avec des stats
+nom_fichier_python="exercise.py"
+contenu_python="""
+def sum(a,b):
+    return(a+b)"""
+nom_fichier_test_python="test_exercise.py"
+contenu_test="""
+import pytest
+import exercise.py as Ex
+
+def test_sum():
+    assert ( Ex.sum(a,b)== a+b)"""
+def combien_fichier(id_candidat,dateEntretien):
+    nombre_fichiers=int((np.random.exponential(9,1))//1)
+    nombre_fichiers=1
+    fichiers=[]
+    for num_fichier in range(nombre_fichiers):
+        id=str(id_candidat)+str(num_fichier//100)+str(num_fichier//10)+str(num_fichier//1)
+        nom=nom_fichier_python
+        contenu=contenu_python
+        nomTest=nom_fichier_test_python
+        contenu_Test=contenu_test
+        dateUpload=grd.date_depot_fichier(dateEntretien)
+        stats={"functionsCount": rd.randint(0,100),"commentCount": rd.randint(0,100),
+                "variableNameQuality": rd.random(),"duplicate": As_tu_copier(),
+                "compteRendu":"Bon code!",'dateUpload' :dateUpload}
+        fichiers.append({'id':id,'nom':nom,'contenu':contenu,'nomTest':nomTest,'contenu_Test':contenu_Test,'stats':stats})
+    return(fichiers)
+
+#CHEATING
+def As_tu_copier():
+    nombre_similarity=int((np.random.exponential(9,1))//1)#nombre de triches
+    similarities=[]
+    for cheat in range(nombre_similarity):
+        id_candidat=rd.randint(0,nombre_candidats-1)
+        contenu_triche="a+b"
+        similarities.append({'id':id_candidat,'similarity':contenu_triche})
+    return(similarities)
+
+#Etat de la candidature
+etats=['Postulé','Exercice donné','Code en cours de vérification','Fin de candidature','Refus','Recruté']
+
+
+#On génère un candidat de façon random
+def creation_candidat(id_candidat):
+    nom=liste_noms[rd.randint(0,len(liste_noms)-1)]
+    prenom=liste_prenoms[rd.randint(0,len(liste_prenoms)-1)]
+    dateNaissance,dateEntretien=grd.dates_aleatoires_naissance_entretien()
+    lieuNaissance=liste_villes[rd.randint(0,len(liste_villes)-1)]
+    lieuEntretien=liste_lieu_rdv[rd.randint(0,len(liste_lieu_rdv)-1)]
+    fichiers=combien_fichier(id_candidat,dateEntretien)
+    level=rd.randint(1,5)
+    etat=rd.choice(etats)
+    return ({'id':id_candidat,'nom':nom,'prenom':prenom,'dateNaissance':dateNaissance,
+            'lieuNaissance':lieuNaissance,'dateEntretien':dateEntretien,'lieuEntretien':lieuEntretien,
+            'fichiers':fichiers,'etat':etat,'metrics':{'level':level}})
+
+def creation_n_candidats(nombre_candidats):
+    candidats=[]
+    for id_candidat in range(nombre_candidats):
+        candidats.append(creation_candidat(id_candidat))
+    return candidats
+print(creation_n_candidats(nombre_candidats))
---
+++ visualizer/db/get_random_date.py
@@ -0,0 +1,53 @@
+import time
+import random
+
+"""on écrit un programe qui crée des dates aléatoires entre la date start et la date end
+1970 correspond à l'origine des temps (on ne peut pas atteindre des dates avant"""
+
+def date_aleatoire_entre_start_end(start_date, end_date):
+    format = '%d/%m/%Y %I:%M %p'
+    nb = random.random()
+        #on passe les chaînes de caractères en datetime
+        #il faut qu'elles soient données dans le format spécifié
+    start_date = time.mktime(time.strptime(start_date, format))
+    end_date = time.mktime(time.strptime(end_date, format))
+        #on calcule la nouvelle date
+    date = start_date + nb * (end_date - start_date)
+        #on la transforme en string
+    return time.strftime(format, time.localtime(date))
+
+
+#print(date_aleatoire_entre_start_end("01/01/1970 01:30 PM", "31/12/1998 11:59 PM"))
+#print(type(randomDate("1/1/2008 1:30 PM", "1/1/2009 4:50 AM", random.random())))
+
+"""PROBLEME: l'origine des temps est située au 01/01/1970.
+Pour atteindre des dates inférieures, il faut ruser (sinon "mktime argument out of range")
+1970 correspond à l'origine des temps"""
+
+
+
+"""on écrit un programe qui crée une date de naissance (entre 1970 et 1998) et une date d'entretien
+(au mois de septembre 2018) aléatoires"""
+
+def dates_aleatoires_naissance_entretien():
+    dateNaissance = date_aleatoire_entre_start_end("01/01/1970 01:30 PM", "31/12/1998 11:59 PM")[:10]
+        #on récupère seulement la date
+    jour_entretien = random.randint(0,30)
+    dateEntretien = str(jour_entretien) + "/09/2018"
+    return [dateNaissance, dateEntretien]
+
+#print(dates_aleatoires_naissance_entretien())
+
+"""on crée une fonction qui crée aléatoirement une date de dépôt de fichiers, comprise dans les 5 jours
+suivant la date de l'entretien"""
+def date_depot_fichier(date_entretien):
+    jour_entretien = int(date_entretien[:2])
+    jour_depot = random.randint(0,5)
+    if jour_entretien+jour_depot <= 30:
+        date_depot = str(jour_entretien+jour_depot) + "/09/2018"
+    else:
+        date_depot = str((jour_entretien+jour_depot)%30) + "/10/2018"
+    return date_depot
+
+date_entretien = "29/09/2018"
+#print(date_depot_fichier(date_entretien))
