ed51f2331436df406c599f4713819b6e93fd3c4f
CamilleBouyssel <camille.bouyssel@student-cs.fr>
2018-11-19 14:49:40 +0100

    Création json

--- visualizer/db/gen.py
+++ visualizer/db/gen.py
@@ -1,10 +1,13 @@
 import json
 import random as rd
+import numpy as np
 
-
+#On créer des candidats
+nombre_candidats=40
 #On formalise l'ecriture des noms et prenoms
 def formalise_nom(nom):
     nom=nom.replace(' ','')
+    nom=str(nom)
     maj=nom[0].upper()
     min=nom[1:].lower()
     return(maj+min)
@@ -37,8 +40,57 @@ for ligne in noms:
     while ligne[car] != '\t':
         car+=1
     liste_noms.append(formalise_nom(ligne[:car]))
+#On rècupère une liste de villes
 
-#On génère un nom et un prenom de façon random
+villes=open("villes_france.csv", "r").readlines()
+liste_villes=[]
+for ville in villes:
+    rang_premiere_virgule=0
+    while ville[rang_premiere_virgule]!=',':
+        rang_premiere_virgule+=1
+    rang_min=rang_premiere_virgule+1
+    while ville[rang_min]!=',':
+        rang_min+=1
+    rang_max=rang_min+1
+    while ville[rang_max]!=',':
+        rang_max+=1
+    ville=ville[rang_min+1:rang_max]
+    ville=ville[1:-1]#On enlève les doubles guillemets
+    liste_villes.append(formalise_nom(ville))
+
+#Liste de lieux de RDV
+liste_lieu_rdv=['Eiffel','Bouygues','Breguet']
+
+
+#On créer des fichiers avec des stats
+nom_fichier_python="exercise.py"
+contenu_python="""
+def sum(a,b):
+    return(a+b)"""
+nom_fichier_test_python="test_exercise.py"
+contenu_test="""
+import pytest
+import exercise.py as Ex
+
+def test_sum():
+    assert ( Ex.sum(a,b)== a+b)"""
+functionsCount=rd.randint(0,100)
+commentCount=rd.randint(0,100)
+variableNameQuality=rd.random()
+compteRendu="Bon code!"
+level=rd.randint(1,5)
+def As_tu_copier():
+    nombre_similarity=int((np.random.exponential(9,1))//1)#nombre de triches
+    similarities=[]
+    for cheat in range(nombre_similarity):
+        id_candidat=rd.randint(0,nombre_candidats-1)
+        contenu_triche="a+b"
+        similarities.append({'id':id_candidat,'similarity':contenu_triche})
+    return(similarities)
+print(As_tu_copier())
+
+#On génère un candidat de façon random
 nom=liste_noms[rd.randint(0,len(liste_noms)-1)]
 prenom=liste_prenoms[rd.randint(0,len(liste_prenoms)-1)]
-
+ville_de_naissance=liste_villes[rd.randint(0,len(liste_villes)-1)]
+lieu_rdv=liste_lieu_rdv[rd.randint(0,len(liste_lieu_rdv)-1)]
