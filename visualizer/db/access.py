7b053d335aa93639c79b5951804f554efdaab3a4
“Hugo <hugo.sonnery@student-cs.fr>
2018-11-19 11:52:31 +0100

    Ajout de fonctions de mise à jour et d'affichage humain de la base de données JSON

--- visualizer/db/access.py
+++ visualizer/db/access.py
@@ -1,12 +1,38 @@
 import json
+import os
 
-def getData(fileName="input.json"):
-    inputFile = open(fileName)
-    inputStr = inputFile.read()
-    inputData = json.loads(inputStr)
+
+
+def getData(fileName = "input.json"):
+    # Fonction ouvrant le fichier JSON contenant la liste 
+    # des candidats et renvoyant une liste de dictionnaires, 
+    # chacun contenant les informations relatives à un candidat
+    with open(fileName, "r+") as inputFile:
+        inputStr = inputFile.read()
+        inputData = json.loads(inputStr)
     
-    resultat = inputData["candidats"]
+        data = inputData["candidats"]
 
-    return resultat
+        inputFile.close()
 
-print(getData())
+        return data
+
+
+
+def prettyPrint(fileName = "input.json"):
+    # Fonction affichant humainement le fichier JSON dans le terminal
+    data = getData(fileName)
+
+    print(json.dumps(data, indent=4, sort_keys=False))
+
+
+
+def update(data = [], fileName = "input.json"):
+    # Après de multiples modifications sur la variable data 
+    # contenant l'équivalent du fichier JSON, cette fonction 
+    # met à jour les modifications en les enregistrant dans le fichier JSON en question
+    os.remove(fileName)
+
+    with open(fileName, "w") as inputFile: 
+        json.dump(data, inputFile, indent=4)
+        inputFile.close()
