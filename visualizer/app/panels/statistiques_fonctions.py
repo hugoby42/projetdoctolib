265c192036d5270ac64dbbc6b08a25f10c8d9568
clarani <45000526+clarani@users.noreply.github.com>
2018-11-23 16:55:18 +0100

    Update statistiques_fonctions.py

--- visualizer/app/panels/statistiques_fonctions.py
+++ visualizer/app/panels/statistiques_fonctions.py
@@ -1,14 +1,20 @@
 from datetime import datetime
 
 
-#fonction qui calcule le nb de jours moyen entre l'entretien et le dépôt pour un candidat
+# fonction qui calcule le nb de jours moyen entre l'entretien et le dépôt pour un candidat
+
 def nb_jours_avant_depot(candidat):
+    # on transforme les chaînes de caractères en objets datetime
     date_entretien = datetime.strptime(candidat['dateEntretien'], '%d/%m/%Y')
+    
     nb_fichiers = len(candidat['fichiers'])
+    # s'il n'y a pas de fichier, on renvoit 0
     if nb_fichiers == 0:
         return 0
+    
     else:
         nb_jours_avant_depot_moyen = 0
+        # On fait la moyenne du nb de jours entre l'entretien et le dépôt d'un exercice par fichier pour le candidat
         for fichier in candidat["fichiers"]:
             date_depot = datetime.strptime(fichier['stats']['dateUpload'], '%d/%m/%Y')
             if date_depot != date_entretien:
@@ -16,11 +22,16 @@ def nb_jours_avant_depot(candidat):
         return nb_jours_avant_depot_moyen/nb_fichiers
 
 
-# Fonction effectuant les statistiques selon chacun des aspects utilisés pour les statistiques
+# Fonction effectuant les statistiques selon chacun des aspects utilisés pour les statistiques (i.e. le nb de fonctions,
+# de commentaires, de fraudes, et la moyenne de la qualité des variables)
+
 def compteur_statistique(candidat):
+    # si le candidat n'a fourni aucun fichier, on renvoie 0
     nb_fichiers=len(candidat['fichiers'])
     if nb_fichiers == 0:
         return 0
+    
+    # sinon, on fait la moyenne de chaque caractéristique par fichier pour le candidat
     nb_fonctions=0
     nb_comments=0
     qualite_variable=0
